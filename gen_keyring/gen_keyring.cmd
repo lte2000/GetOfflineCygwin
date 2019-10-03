f79d43190f59f7480573d66871e8e163bbd0199a
ngn2000 <ngn2000@126.com>
2019-10-03 20:55:04 +0800

    remove svn id

--- gen_keyring/gen_keyring.cmd
+++ gen_keyring/gen_keyring.cmd
@@ -1,8 +1,4 @@
 @echo off
-REM -----------------------------------------------------------------
-REM Description: generate keyring for package validation
-REM $Id: gen_keyring.cmd,v 1.1 2011-04-23 12:34:45 jyliu Exp $
-REM -----------------------------------------------------------------
 
 @gpg.exe --no-default-keyring --keyring .\cygwin.gpg --import cygwin.sig
 rem gpgv --keyring .\test.pgp setup.ini.sig
